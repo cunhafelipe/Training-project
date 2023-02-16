a9775b064f36969f2af56cf0bb7250c0454a490f
cunhafelipe <felipe.cunha2001@hotmail.com>
2023-02-16 17:39:20 -0300

    adding controller to login

--- controllers/controller.js
+++ controllers/controller.js
@@ -1,7 +1,11 @@
+const { request } = require("express");
 const express = require("express");
 
 module.exports = {
   home: (_request, response) => {
     response.render("home");
+  },
+  login: (request, response) => {
+    response.render("login");
   },
 };
