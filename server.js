99763daa1d040c26c4776305b894b98f85947956
cunhafelipe <felipe.cunha2001@hotmail.com>
2023-02-16 19:44:09 -0300

    config route server

--- server.js
+++ server.js
@@ -6,5 +6,6 @@ server.set("view engine", "ejs");
 
 server.get("/", userController.home);
 server.get("/login", userController.login);
+server.get("/register", userController.register);
 
 server.listen(4001, () => console.log("Servidor executado na porta 4001"));
